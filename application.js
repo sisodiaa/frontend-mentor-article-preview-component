adc76cd90422395d89f72546641c51997cea8b40
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2021-12-17 18:17:14 +0530

    Make design responsive for mobile and desktop devices

--- application.js
+++ application.js
@@ -1,35 +1,29 @@
-var shareButtons = document.querySelectorAll(".article__button");
-var tooltipButton = document.querySelector(".article__footer .article__button");
-var articleShare = document.querySelector(".article__share");
-var articleFooter = document.querySelector(".article__footer");
+document.addEventListener("DOMContentLoaded", () => {
+  var shareButtons = document.querySelectorAll(".article__button");
+  var tooltipButton = document.querySelector(".article__footer .article__button");
+  var articleShare = document.querySelector(".article__share");
+  var articleFooter = document.querySelector(".article__footer");
 
-var viewPortWidth = () => {
-  return window.innerWidth / parseFloat(
-    getComputedStyle(
-      document.querySelector('html')
-    )['font-size']
-  )
-}
+  if (window.innerWidth <= 803) {
+    shareButtons.forEach((shareButton) => {
+      shareButton.addEventListener('click', (event) => {
+        event.preventDefault();
+        event.stopPropagation();
 
-if (viewPortWidth() <= 50.3) {
-  shareButtons.forEach((shareButton) => {
-    shareButton.addEventListener('click', (event) => {
+        if (articleFooter.classList.contains("active")) {
+          articleFooter.classList.remove("active");
+          articleShare.classList.add("active");
+        } else {
+          articleShare.classList.remove("active");
+          articleFooter.classList.add("active");
+        }
+      });
+    });
+  } else {
+    tooltipButton.addEventListener('click', (event) => {
       event.preventDefault();
       event.stopPropagation();
-
-      if (articleFooter.classList.contains("active")) {
-        articleFooter.classList.remove("active");
-        articleShare.classList.add("active");
-      } else {
-        articleShare.classList.remove("active");
-        articleFooter.classList.add("active");
-      }
+      tooltipButton.classList.toggle("tooltip");
     });
-  });
-} else {
-  tooltipButton.addEventListener('click', (event) => {
-    event.preventDefault();
-    event.stopPropagation();
-    tooltipButton.classList.toggle("tooltip");
-  });
-}
+  }
+});
