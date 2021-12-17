2a3d10c8683bba5c5fc1b9718ba4d39ad36d5f76
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2021-12-17 17:48:49 +0530

    Add tooltip for desktop view

--- application.js
+++ application.js
@@ -1,4 +1,5 @@
 var shareButtons = document.querySelectorAll(".article__button");
+var tooltipButton = document.querySelector(".article__footer .article__button");
 var articleShare = document.querySelector(".article__share");
 var articleFooter = document.querySelector(".article__footer");
 
@@ -10,22 +11,25 @@ var viewPortWidth = () => {
   )
 }
 
-shareButtons.forEach((shareButton) => {
-  shareButton.addEventListener('click', (event) => {
-    event.preventDefault();
-    event.stopPropagation();
-    console.log('Sharing...');
+if (viewPortWidth() <= 50.3) {
+  shareButtons.forEach((shareButton) => {
+    shareButton.addEventListener('click', (event) => {
+      event.preventDefault();
+      event.stopPropagation();
 
-    if (viewPortWidth() <= 50.3) {
       if (articleFooter.classList.contains("active")) {
-        console.log("In footer");
         articleFooter.classList.remove("active");
         articleShare.classList.add("active");
       } else {
-        console.log("In Share");
         articleShare.classList.remove("active");
         articleFooter.classList.add("active");
       }
-    }
+    });
   });
-});
+} else {
+  tooltipButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    tooltipButton.classList.toggle("tooltip");
+  });
+}
